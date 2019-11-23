8dcc3f0c5d704959344a98071853a17fe4854538
Roôana Subtil <rosubtil@gmail.com>
2019-11-23 14:07:45 -0300

    Tratando Pagina Login.php

---
+++ FrontEnd/login.php
@@ -0,0 +1,32 @@
+<?php
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://localhost/ProjetoFinal/squad-4-vhsys-1/public/api/v1/requestToken",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "email=".$_POST['email']."&password=".$_POST['password'],
+  CURLOPT_HTTPHEADER => array(
+    "Accept: application/json",
+    "Content-Type: application/x-www-form-urlencoded",
+    "Postman-Token: cdf3b5e9-c2d4-4d86-9c86-c58949dc029a",
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}

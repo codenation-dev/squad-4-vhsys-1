ff7500e39f26e2745a67d50613aea26a4f9b7cdd
Roôana Subtil <rosubtil@gmail.com>
2019-12-01 00:23:03 -0300

    Botão Detalhes

--- FrontEnd/logs.php
+++ FrontEnd/logs.php
@@ -14,7 +14,11 @@ include 'listLogs.php';
 <html lang="br">
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
     <title> Central de Erros </title>
 </head>
 <body>
@@ -28,8 +32,8 @@ include 'listLogs.php';
             <div style="text-align:right;" ><a href="cadastroLogs.php" style="color: white; text-decoration:none">Cadastrar novo Erro</a></div>
         </div>
     </div>
-    <div style="text-align: left"> 
-        Ol&aacute; Usu&aacute;rio X 
+    <div style="text-align: left">
+        Ol&aacute; Usu&aacute;rio X
         <br>
         <br>
         Seu Token &eacute;: <?php echo $_SESSION['Token']; ?>
@@ -41,14 +45,15 @@ include 'listLogs.php';
             <thead>
             <tr>
                 <th scope="col">Tipo</th>
-                <th scope="col">Descri&ccedil;&atilde;o</th>
+                <th scope="col">Titulo</th>
                 <th scope="col">Ambiente</th>
                 <th scope="col">Status</th>
                 <th scope="col">Usu&aacute;rio</th>
+                <th scope="col">Detalhes</th>
             </tr>
             </thead>
             <tbody>
-  
+
         <?php foreach($response['data'] as $result){ ?>
            <tr>
                 <th scope="row"><?php echo $result['level'] ?></th>
@@ -56,6 +61,31 @@ include 'listLogs.php';
                 <td><?php echo $result['ambience'] ?></td>
                 <td><?php echo $result['status'] ?></td>
                 <td><?php echo $result['user_created'] ?></td>
+                <td>
+                    <div class="container">
+                       <button type="button" href="<?php echo $result['log']; ?>" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Detalhes</button>
+                   </div>
+                    <div class="modal fade" id="myModal">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title"><?php echo $result['log'] ?></h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="fetched-data">
+                                        <?php echo $result['ambience'] ?>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </td>
             </tr>
         <?php } ?>
             </tbody>
